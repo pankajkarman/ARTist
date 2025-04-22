f3cd55ed4fec84874b0d1f289f73df98b5392136
Pankaj Kumar <pankaj.kmr1990@gmail.com>
2025-04-22 11:57:27 +0200

    adde tri_plot method to visualization

--- artist/utils.py
+++ artist/utils.py
@@ -15,6 +15,33 @@ class PlotAccessor(object):
         print('Wait: Figures are on the way.')
         pass
     
+    def tri_plot(self, ax, cmap='coolwarm', levels=10, add_colorbar=True, map_extent=None, projection=None):
+        if projection:
+            if projection == ccrs.PlateCarree():
+                tcf = ax.tricontourf(self._obj.clon, self._obj.clat, self._obj, cmap=cmap, levels=levels)
+            else:
+                try:
+                    mproj = projection.transform_points(
+                        ccrs.PlateCarree(),
+                        self._obj.clon,
+                        self._obj.clat
+                    )
+                    x, y =  mproj[:, 0],  mproj[:, 1]
+                    tcf = ax.tricontourf(x, y, self._obj, cmap=cmap, levels=levels)
+                except:
+                    ax.set_global()
+                    tcf = ax.tricontourf(self._obj.clon,  self._obj.clat, self._obj,
+                            levels=levels,
+                            cmap=cmap,
+                            transform=ccrs.PlateCarree())
+
+        else:
+            tcf = ax.tricontourf(self._obj.clon, self._obj.clat, self._obj, cmap=cmap, levels=levels)
+
+        if add_colorbar:
+            cbar = plt.colorbar(tcf, orientation='vertical', pad=0.05)
+            cbar.set_label(self._obj.attrs['standard_name'])
+        return ax
     
     
 def add_grid(gridfile, ltranslon=True):
